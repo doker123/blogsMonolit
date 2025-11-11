d9e0a00ac2a79ae1b354e12df862b7b021a0fa73
doker123 <dodo2larin@gmail.com>
2025-11-11 13:04:46 +0700

    поправил главную

--- core/views.py
+++ core/views.py
@@ -4,7 +4,8 @@ from .models import Post
 
 def home(request):
     posts = Post.objects.select_related('author').order_by('-created_at')
-    paginator = Paginator(posts, 50)  # 50 постов на страницу
+    paginator = Paginator(posts, 50)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
+
     return render(request, 'core/home.html', {'page_obj': page_obj})
