2e5a2895c22e8bdd41280221b5646680f837f2dd
doker123 <57056177+doker123@users.noreply.github.com>
2025-12-04 20:52:29 +0700

    Delete core directory

--- core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from core.models import Profile, Post, Comment, Like
-
-admin.site.register(Profile)
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Like)
